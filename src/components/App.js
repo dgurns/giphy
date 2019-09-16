8499b3a9493cf137f24591e59d1166d66fc81a6c
Dan Gurney <gurney.dan@gmail.com>
2019-09-16 23:00:20 +0100

    Start roughing out styles for App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className="app">
+      <h1>Giphy Search</h1>
+      <div className="app__search">
+        <input placeholder="Enter search term..." />
+        <button>Search</button>
+      </div>
+      <div className="app__results-container">
+        <div className="app__result-item" />
+        <div className="app__result-item" />
+        <div className="app__result-item" />
+        <div className="app__result-item" />
+        <div className="app__result-item" />
+        <div className="app__result-item" />
+        <div className="app__result-item" />
+        <div className="app__result-item" />
+        <div className="app__result-item" />
+        <div className="app__result-item" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
